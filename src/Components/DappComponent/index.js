01560169b5ec3f3b121c13e92107918c5cc09d26
Shabir Ahmad <shabirahmad14217@gmail.com>
2022-11-23 17:15:04 +0530

    integrated smart contract

--- src/Components/DappComponent/index.js
+++ src/Components/DappComponent/index.js
@@ -7,11 +7,26 @@ function DappComponent() {
   let web3;
   const [errorMsg, setErrorMsg] = useState("");
   const [inventoryValue, setInventoryValue] = useState("");
+  const [myDonoutsValue, setMyDonoutsValue] = useState('');
+  const [accountBalance, setAccountBalance] = useState('');
+  const [accountAddress, setAccountAddress] = useState('');
+
+
+//   async function getAccount() {
+//     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//     const account = accounts[0];
+//     showAccount.innerHTML = account;
+//   }
   const handleButtonClick = async () => {
     if (typeof window !== undefined && typeof window.ethereum !== undefined) {
       try {
+        // getAccount();
         await window.ethereum.request({ method: "eth_requestAccounts" });
         web3 = new Web3(window.ethereum);
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        window.innerHTML = account;
+        getDodonutHandler();
       } catch (err) {
         console.log(err.message);
       }
@@ -31,6 +46,20 @@ function DappComponent() {
       .call();
     setInventoryValue(inventory);
   };
+
+  const getDodonutHandler = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const count = await vmContract.methods.donutBalances(accounts[0]).call();
+    console.log('account addres',accounts);
+    let accountBalance = await web3.eth.getBalance(accounts[0]);
+    accountBalance = accountBalance.toString();
+    console.log('update balance',`${accountBalance}`+1 )
+    const updatedAccoutBalance = await web3.utils.fromWei(accountBalance,'ether');
+    console.log('updated balnce', updatedAccoutBalance);
+    setAccountAddress(accounts[0]);
+    setAccountBalance(updatedAccoutBalance);
+    setMyDonoutsValue(count);
+  };
   return (
     <>
       <div
@@ -46,6 +75,21 @@ function DappComponent() {
       <section>{errorMsg && <p>errorMsg</p>}</section>
       <section className="App">
         <p>Total no of donouts we are having {inventoryValue}</p>
+      </section>
+
+      <section className="App">
+        <p>Account Address {accountAddress}</p>
+      </section>
+      <section className="App">
+        <p>Account Balance {accountBalance}</p>
+      </section>
+      <section className="App">
+        <p>My donouts Value {myDonoutsValue}</p>
+      </section>
+
+      <section className="App">
+        <p>Buy donouts</p>
+        <inpu type="text" ></inpu>
       </section>
     </>
   );
